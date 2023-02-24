a19f20155c0b7386e9eb673c13fb4bddfd8a5404
ChristinaLear-coder <115825078+ChristinaLear-coder@users.noreply.github.com>
2023-02-24 23:50:28 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1> React Weather App</h1>
-        <a
-          className="App-link"
-          href="##"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Coded by Christina Lear, open-sourced on GitHub
-        </a>
-      </header>
+    <div className="container">
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1> React Weather App</h1>
+          <a
+            className="App-link"
+            href="##"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Coded by Christina Lear, open-sourced on GitHub
+          </a>
+        </header>
+      </div>
     </div>
   );
 }
