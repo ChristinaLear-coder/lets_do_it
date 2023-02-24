4f568d9d379fd5a826d97027198f910949a98a64
ChristinaLear-coder <115825078+ChristinaLear-coder@users.noreply.github.com>
2023-02-25 01:40:16 +0200

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,44 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city"
-              className="form-control"
-            />
+  const [ready, setReady] = useState(false);
+  const [temperature, setTemperature] = useState(null);
+  function handleResponse(response) {
+    console.log(response.data);
+    setTemperature(response.data.main.temp);
+    setReady(true);
+  }
+
+  if (ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                className="form-control"
+              />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="Search" className="btn btn-primary" />
+            </div>
           </div>
-          <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+        </form>
+        <h1>New York</h1>
+        <ul>
+          <li>Wednesday</li>
+          <li> Mostly Cloudy</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
+            <div className="clearfix">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+                alt="mostly cloudy"
+                className="float-left"
+              />
+              <span className="temperature">{Math.round(temperature)}</span>
+              <span className="unit"> C </span>C
+            </div>
           </div>
-        </div>
-      </form>
-      <h1>New York</h1>
-      <ul>
-        <li>Wednesday</li>
-        <li> Mostly Cloudy</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
-            alt="mostly cloudy"
-          />
-          6°C
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 2%</li>
-            <li>Humidity: 81%</li>
-            <li>Wind: 3 km/h</li>
-          </ul>
+
+          <div className="col-6">
+            <ul>
+              <li>Precipitation: 2%</li>
+              <li>Humidity: 81%</li>
+              <li>Wind: 3 km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "141e8b17b5d815089695c5174ac04988";
+    let city = "New York";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading the app...";
+  }
 }
