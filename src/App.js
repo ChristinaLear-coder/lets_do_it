b2efc6ebe33e90e2b460a8fef3154f62d284aa46
ChristinaLear-coder <115825078+ChristinaLear-coder@users.noreply.github.com>
2023-02-24 23:32:35 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>React Weather App</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="##"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Coded by Christina Lear, open-sourced on GitHub
         </a>
       </header>
     </div>
   );
 }
-
-export default App;
